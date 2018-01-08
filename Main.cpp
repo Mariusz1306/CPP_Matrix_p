a3f35e1750e3b732e1f046c91c2f9ce152c63f86
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2018-01-08 22:07:39 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <fstream>
+#include "rcmatrix.h"
+
+int main(){
+    CMatrix a;
+    CMatrix b(5, 5, 1);
+    CMatrix c(5, 5, 1, 2);
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
+    return 0;
+}
